a34587045c926b487597ff3d6c8e3274685aa244
dblock <dblock@dblock.org>
2014-08-25 15:00:14 -0400

    Upgraded RSpec expectation syntax.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 $:.unshift File.expand_path('..', __FILE__)
 $:.unshift File.expand_path('../../lib', __FILE__)
+
 require 'simplecov'
 require 'omniauth-artsy'
 require 'rspec'
 require 'webmock/rspec'
+
+RSpec.configure do |config|
+  config.raise_errors_for_deprecations!
+end
